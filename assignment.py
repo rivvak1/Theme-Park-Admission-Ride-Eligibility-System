a5e6e181a43dc0ca457eb18d6419b2a199b3ea6f
Angie Kireeva <angie@Angies-MacBook-Air-3.local>
2026-09-17 13:40:18 -0700

    Theme park elig system 2.0

--- assignment.py
+++ assignment.py
@@ -3,7 +3,8 @@ print("We are going to help you set up your tickets.")
 print("And pick which rides you'd like to go to.")
 
 # questions -------------------
-
+# just variables with inputs
+# basically where the user inputs their answer which is then used to calculate everything
 guest_name = input("What is your name?:")
 age = int(input("What is your age?:"))
 height = float(input("How tall are you? (inch):"))
@@ -11,8 +12,10 @@ ticket_type = input("Did you buy a regular or premium ticket?:")
 park_member = input("Are you a park member? (yes/no):")
 visiting_adult = input("Are you visiting with an adult? (yes/no):")
 visit_time = input("Are you visiting in the morning or evening?:")
-# printing --------------
 
+# printing --------------
+# prints of basic guest information
+# just takes the inputs answer and turns it into pretty text
 print("Guest Name:", guest_name)
 print("Age:", age)
 print("Height:", height)
@@ -31,7 +34,8 @@ Age   Price
 """
 
 # calculations ------------------
-
+# this is filled with if statements
+# our def line defines the function, which has the if statements in it
 def calculate_admission(age):
     if age <= 4:
         return 0
@@ -41,7 +45,7 @@ def calculate_admission(age):
         return 30
     else:
         return 20
-
+# this function controls the ticket price per age
 admission_price = calculate_admission(age)
 print ("Admission Price Before Discount:", admission_price)
 
@@ -56,6 +60,7 @@ def calculate_discount(admission_price, park_member, visit_time):
         return admission_price  - 5
     else:
         return admission_price - 0
+# this one controls what discounts happen and how much 
 after_discount = calculate_discount(admission_price, park_member, visit_time)
 
 def below_zero(after_discount):
@@ -63,7 +68,8 @@ def below_zero(after_discount):
         return 0
     else:
         return after_discount
-
+# this one makes it so if the guest's ticket price 
+# is 0, and they are eligible for a discount, they dont get a negative number as a price
 full_price = below_zero(after_discount)
 
 print("Final Admission Price:", full_price)
@@ -79,7 +85,7 @@ def ride_level(age, height):
             return "Kiddie Rides"
     else:
         return "No Rides"
-
+# this one controls what type of ride the user is eligible for and the conditions for the ride requirement
 ride_access = ride_level(age, height)
 
 print("Ride level:", ride_access)
@@ -89,7 +95,7 @@ def check_supervision(age , visiting_adult):
         return "Adult Required"
      else:
           return "Approved"
-
+# this one makes sure that if you input your age as under 13, you need to have adult supervision to enter
 adult_check = check_supervision(age , visiting_adult)
 
 print("Supervision Status:", adult_check)
@@ -99,7 +105,7 @@ def check_premium(ticket_type):
           return "PREMIUM BONUS (free snack and priority ride access!)"
      else:
           return "REGULAR TICKET"
-
+# this one checks if your ticket is regular or premium, and if its premium you get perks
 premium_check = check_premium(ticket_type)
 
 print("Premium Ticket Status:", premium_check)
